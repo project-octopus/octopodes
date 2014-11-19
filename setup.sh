08c6f5d5a027ee04923ba673d569f28bf596b9a2
Christopher Adams <chris@raysend.com>
2014-11-19 18:07:09 +0800

    Fix design doc path

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ curl -vX DELETE http://localhost:5984/collection-data-works
 curl -vX PUT http://localhost:5984/collection-data-works
 
 echo 'adding design document'
-curl -vX PUT http://localhost:5984/collection-data-works/_design/all -d @design-doc.json
+curl -vX PUT http://localhost:5984/collection-data-works/_design/all -d @db/design-doc.json
