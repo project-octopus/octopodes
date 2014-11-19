0b4911c1f1bd577fad2b5ed3d914a19309258b7e
Christopher Adams <chris@raysend.com>
2014-11-19 19:37:25 +0800

    Improve template layout with bootstrap

--- lib/templates.rb
+++ lib/templates.rb
@@ -15,7 +15,7 @@ class CollectionTemplate
       <!DOCTYPE html>
       <html>
       <head>
-        <title>Reviews</title>
+        <title>Octopus Project</title>
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.0/paper/bootstrap.min.css">
         <style>
           body {
@@ -34,57 +34,61 @@ class CollectionTemplate
       </head>
       <body>
       <div class="container-fluid">
-      <div class="row">
-      <div class="col-lg-12">
-      <h1><a href="<%= @href %>">Reviews</a></h1>
-      <% unless @error.nil? %>
-        <div class="error">
-          Error: <%= @error.title %><br/>
-          <%= @error.message %>
-        </div>
-      <% end %>
-      <% for @item in @items %>
-        <hr/>
-        <dl>
-          <dt>
-          <a href="<%= @item.href %>">
-            <%= @item.href %>
-          </a>
-          </dt>
-            <% for link in @item.links %>
-              <dd><%= link.prompt %>: <%= link.href %></dd>
+        <div class="row">
+          <div class="col-lg-12">
+            <h1><a href="<%= @href %>">Reviews</a></h1>
+            <% unless @error.nil? %>
+              <div class="error">
+                Error: <%= @error.title %><br/>
+                <%= @error.message %>
+              </div>
             <% end %>
-            <% for datum in @item.data %>
-              <% unless datum.value.nil? %>
-                <dd><%= datum.prompt %>: <%= datum.value %></dd>
-              <% end %>
+            <hr/>
+            <% unless @inputs.nil? %>
+            <form role="form" method="POST" class="form-horizontal">
+            <% for @input in @inputs %>
+              <div class="form-group">
+              <label class="col-sm-2 control-label" for="<%= @input.name %>">
+                <%= @input.prompt %>
+              </label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" name="<%= @input.name %>" autocomplete="off"/>
+              </div>
+              </div>
             <% end %>
-        </dl>
-        </body>
-      <% end %>
-      <hr/>
-      <% unless @inputs.nil? %>
-      <form role="form" method="POST" class="form-horizontal">
-      <% for @input in @inputs %>
-        <div class="form-group">
-        <label class="col-sm-2 control-label" for="<%= @input.name %>">
-          <%= @input.prompt %>
-        </label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" name="<%= @input.name %>" autocomplete="off"/>
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-8">
+                <input class="btn btn-default" type="submit" value="Submit"/>
+              </div>
+            </div
+            </form>
+            <% end %>
+          </div>
         </div>
+        <div class="row">
+          <div class="col-sm-offset-2 col-sm-8">
+            <% for @item in @items %>
+              <dl>
+                <dt>
+                <a href="<%= @item.href %>">
+                  <%= @item.href %>
+                </a>
+                </dt>
+                  <% for link in @item.links %>
+                    <dd><%= link.prompt %>: <%= link.href %></dd>
+                  <% end %>
+                  <% for datum in @item.data %>
+                    <% unless datum.value.nil? %>
+                      <dd><%= datum.prompt %>: <%= datum.value %></dd>
+                    <% end %>
+                  <% end %>
+              </dl>
+              <hr/>
+              </body>
+            <% end %>
+            </div>
+          </div>
         </div>
-      <% end %>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <input class="btn btn-default" type="submit" value="Submit"/>
-        </div>
-      </div
-      </form>
-      <% end %>
-      </div>
-      </div>
-      </div>
       </div>
     }
   end
