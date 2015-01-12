4196ad5b7264b0947e4fcbaa46d4e4543f72db45
Christopher Adams <chris@raysend.com>
2015-01-12 19:20:36 +0800

    Start on draft 2 of design docs

--- db/tmp_designs_helper_2.sh
+++ db/tmp_designs_helper_2.sh
@@ -7,17 +7,17 @@ echo '==========='
 curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/works"
 
 echo ''
-echo 'Total webpages'
-echo '=============='
-curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/webpages"
+echo 'Total published works'
+echo '==========='
+curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/publications"
 
 echo ''
 echo 'Total edits'
-echo '=============='
+echo '==========='
 curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/edits"
 
 echo ''
-echo 'All Domains (ignoring media files)'
+echo 'All Domains'
 echo '=================================='
 curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/domains?group_level=1"
 
@@ -36,12 +36,12 @@ echo 'Everything for The Threatened Swan'
 echo '=================================='
 curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/by_work?reduce=false&startkey=%5B%22works%2F1%22%5D&endkey=%5B%22works%2F1%22%2C%20%7B%7D%5D"
 
-#echo ''
-#echo 'All works'
-#echo '========='
-#curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/works?reduce=false&include_docs=true" | underscore print
+echo ''
+echo 'All Edits for Work 1'
+echo '===================='
+curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/edits?reduce=false&endkey=%5B%22works%2F1%22%5D&startkey=%5B%22works%2F1%22%2C%20%7B%7D%5D&descending=true"
 
-#echo ''
-#echo 'All webpages'
-#echo '========='
-#curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/webpages?reduce=false&include_docs=true" | underscore print
+echo ''
+echo 'All Edits by mzeinstra'
+echo '===================='
+curl -sX GET "http://localhost:5984/project-octopus-v1/_design/all/_view/by_reviewer?reduce=false&startkey=%5B%22users%2Fmzeinstra%22%2C%20%7B%7D%5D&endkey=%5B%22users%2Fmzeinstra%22%5D&descending=true"
